1a48d85ab3188ae7b6368bab922076acef444dc6
noothangimounika <noothangimounika@gmail.com>
2024-11-14 23:01:42 +0530

    Tattoo website testimonials

---
+++ tattoo-project/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+
+import React from 'react'
+import Testimonials from './loginComp/Testimonials';
+
+const App = () => {
+  return (
+    <div>
+      <Testimonials />
+    </div>
+  )
+}
+
+export default App
